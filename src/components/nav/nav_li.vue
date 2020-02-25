7ffb2f2b4a3c5d0bcb1daae49f0f7efbbad315d9
lixianlun <lun96639354@163.com>
2020-02-25 11:42:44 +0800

    updata

--- src/components/nav/nav_li.vue
+++ src/components/nav/nav_li.vue
@@ -1,12 +1,17 @@
 <template>
-  <li class="nav_li">
-    {{item.title}}
-    <span>{{item.num}}</span>
-  </li>
+    <ul class="navul">
+      <li>{{item.title1}}<em>{{num}}</em></li>
+      <li>{{item.title2}}<em>{{num}}</em></li>
+    </ul>
 </template>
 
 <script>
   export default{
+    data(){
+      return{
+        num:"999+",
+      }
+    },
     props:{
       item:{
         type:Object
@@ -16,14 +21,19 @@
 </script>
 
 <style lang="stylus">
-  .nav_li
-    float:left
-    cursor pointer
-    span
-      font-size 5px
-      border-radius 2px
-      text-align center
-      padding 0 5px
-      background #73c9e5
-      color white
+  .navul
+    margin auto 0
+    li
+      cursor pointer
+      margin 5px 0 5px 8px
+      padding  5px 8px
+      em
+        font-style normal
+        margin-left 5px
+        font-size 5px
+        border-radius 2px
+        text-align center
+        padding 0 5px
+        background #73c9e5
+        color white
 </style>
--- src/components/nav/nav_ul.vue
+++ src/components/nav/nav_ul.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="boxnav">
-    <!-- <ul class="hi_ul">
-      <navli v-for="(i,index) in classify" :item="i" :key="i.index" ></navli>
-    </ul> -->
-    <!-- <ul class="hi_ul">
-      <navli v-for="(i,index) in classifd" :item="i" :key="i.index" ></navli>
-    </ul> -->
-    <div class="movie">
-    </div>
+    <ul class="icon_nav">
+      <li>
+        <div class="icon_home state"></div>
+        主页
+      </li>
+      <li>
+        <div class="icon_home ranking"></div>
+        动态
+      </li>
+      <li>
+        <div class="icon_home home"></div>
+        排行榜
+      </li>
+    </ul>
+    <navli v-for="(i,index) in classify" :item="i" :key="i.index" ></navli>
   </div>
 </template>
 
@@ -21,56 +28,30 @@
       return {
         classify: [
           {
-            title: "动画",
-            num: 456
-          },{
-            title:"音乐",
-            num:793
-          },{
-            title:"舞蹈",
-            num:793
-          },{
-            title:"科技",
-            num:793
-          },{
-            title:"生活",
-            num:793
-          },{
-            title:"时尚",
-            num:793
-          },{
-            title:"娱乐",
-            num:793
-          },{
-            title:"放映厅",
-            num:793
-          }
-        ],
-        classifd:[
+            title1:"动画",
+            title2:"番剧"
+          },
           {
-            title:"番剧",
-            num:793
+            title1:"音乐",
+            title2:"国创"
           },{
-            title:"国创",
-            num:793
+            title1:"舞蹈",
+            title2:"游戏"
           },{
-            title:"游戏",
-            num:793
+            title1:"科技",
+            title2:"数码"
           },{
-            title:"数码",
-            num:793
+            title1:"生活",
+            title2:"鬼畜"
           },{
-            title:"鬼畜",
-            num:793
+            title1:"时尚",
+            title2:"广告"
           },{
-            title:"广告",
-            num:793
+            title1:"娱乐",
+            title2:"影视"
           },{
-            title:"影视",
-            num:793
-          },{
-            title:"更多>",
-            num:793
+            title1:"放映厅",
+            title2:"更多>"
           }
         ]
       }
@@ -82,7 +63,25 @@
   .boxnav
     height 64px
     margin-top 20px
-    text-align center
-  .hi_ul
-    display inline-block
+    display flex
+    .icon_nav
+      border-right 1px solid #e7e7e7
+      margin-right 5px
+      padding-right 10px
+      li
+        width 50px
+        float left
+        text-align center
+        margin-right 5px
+      .icon_home
+        width 36px
+        height 36px
+        margin 0 auto 5px
+        border-radius 36px
+        &.home
+          background #1890FF
+        &.state
+          background #FB7299
+        &.ranking
+          background goldenrod
 </style>
